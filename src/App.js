cf4f14dea887cd009853e612ea985ded181386b2
mrCamilo <mcamilo@protonmail.com>
2020-06-15 15:24:16 -0400

    react setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from 'react'
+
+import Header from "./components/Header"
 
 function App() {
   return (
     <div>
-          <h3>Learn React</h3>
+          <Header/>
     </div>
   )
 }
