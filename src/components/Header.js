6de8177f8e66c0f2c67bb7d06a2680568e8e6538
mrCamilo <mcamilo@protonmail.com>
2020-06-23 14:20:18 -0400

    style fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header() {
     return (
-        <header>RTK LOOKUP!</header>
+        <header className="heading">Remembering The Kanji</header>
     )
 }
 
---
+++ src/components/Kanji.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Kanji() {
+    return (
+        <div className="kanji">
+	    <p>hey it me</p>
+	</div>
+    )
+}
+
+export default Kanji
