daf4a4c603d144d02aaf93ff4d37609eace76c9d
mrCamilo <mcamilo@protonmail.com>
2020-06-23 05:27:10 -0400

    structuring app

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# Remembering-The-Kanji-Reference
-Look up all of the entries in 'Remembering the Kanji 1' by James Heisig.
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>RTK LOOKUP!</header>
+    )
+}
+
+export default Header
