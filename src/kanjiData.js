7d80f8b096b73b28405beb2cc4649128671dd244
mrCamilo <mcamilo@protonmail.com>
2020-06-24 10:14:12 -0400

    number 1 and 2 are done

--- src/kanjiData.js
+++ src/kanjiData.js
@@ -4,6 +4,12 @@ const kanjiData = [
 	kanji: 'ichi', 
 	keyword: "one",
 	heisig: "In Chinese characters, the number one is laid on its side, unlike the Roman numeral I which stands upright. As you would expect, it is written from left to right."
+    },
+    {
+	id: 2,
+	kanji: "ni",
+	keyword: "two",
+	heisig: "Like the Roman numeral II, which reduplicates the numeral I, the kanji for two is a simple reduplication of the horizontal stroke that means one. The order of writing goes from above to below, with the first stroke slightly shorter."
     }
 ]
 
