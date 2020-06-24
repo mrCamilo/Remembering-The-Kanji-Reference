3c0edb0dd403586cb470033486765556fc1796a9
mrCamilo <mcamilo@protonmail.com>
2020-06-24 11:53:20 -0400

    updated RTK 1 through 20

--- src/kanjiData.js
+++ src/kanjiData.js
@@ -29,6 +29,96 @@ const kanjiData = [
       keyword: "five",
       heisig: "As with four, we shall postpone learning the primitive elements that make up this character. Note how the general principle we just learned in the preceding frame applies to the writing of the character for five."
     },
+    {
+      id: 6,
+      kanji: '六',
+      keyword: "six",
+      heisig: "The primitives here are top hat and animal legs. Once again, we glide over them until later."
+    },
+    {
+      id: 7,
+      kanji: '七',
+      keyword: "seven",
+      heisig: "Note that the first stroke cuts through the second. This distinguishes seven from the character for spoon (Frame 476), in which the horizontal stroke stops short."
+    },
+    {
+      id: 8,
+      kanji: '八',
+      keyword: "eight",
+      heisig: "Just as the Arabic numeral 8 is composed of a small circle followed by a larger one, so the kanji for eight is composed of a short line followed by a longer line, slanting towards it but not touching it. And just as the lazy 8 is the mathematical symbol for infinity, so the expanse opened up below these two strokes is associated by the Japanese with the sense of an infinite expanse or something all-encompassing."
+    },
+    {
+      id: 9,
+      kanji: '九',
+      keyword: "nine",
+      heisig: "If you take care to remember the stroke order of this kanji, you will not have trouble later keeping it distinct from the kanji for power."
+    },
+    {
+      id: 10,
+      kanji: '十',
+      keyword: "ten",
+      heisig: "Turn this character 45 degrees either way and you have the x used for the Roman numeral ten."
+    },
+    {
+      id: 11,
+      kanji: '口',
+      keyword: "mouth",
+      heisig: "Like several of the first characters we shall learn, the kanji for mouth is a clear pictograph. Since there are no circular shapes in the kanji, the square must be used to depict the circle."
+    },
+    {
+      id: 12,
+      kanji: '日',
+      keyword: "day",
+      heisig: "This kanji is intended to be a pictograph of the sun. Recalling what we said in the previous frame about round forms, it is easy to detect the circle and the big smile that characterize our simplest drawings of the sun - like those yellow badges with the words, Have a nice day!"
+    },
+    {
+      id: 13,
+      kanji: '月',
+      keyword: "month",
+      heisig: "This character is actually a picture of the moon, with the two horizontal lines representing the left eye and mouth of the mythical man in the moon. (Actually, the Japanese see a hare in the moon, but it is a little farfetched to find one in the kanji.) And one month, of course, is one cycle of the moon."
+    },
+    {
+      id: 14,
+      kanji: '月',
+      keyword: "rice field",
+      heisig: "Another pictograph, this kanji looks like a bird's-eye view of a rice field divided into four plots. Be careful when writing this character to get the order of the strokes correct. You will find that it follows perfectly the principle stated in frame 4."
+    },
+    {
+      id: 15,
+      kanji: '目',
+      keyword: "eye",
+      heisig: "Here again, if we round out the corners of this kanji and curve the middle strokes upwards and downwards respectively, we get something resembling an eye."
+    },
+    {
+      id: 16,
+      kanji: '古',
+      keyword: "old",
+      heisig: "The primitive elements that compose this character are ten and mouth, but you may find it easier to remember it as a pictograph of a tombstone with a cross on top. Just think back to one of those graveyards you have visited, or better still, used to play in as a child, with old inscriptions on the tombstones. This departure from the primitive elements in favor of a pictograph will take place now and again at these early stages, and almost never after that. So you need not worry about cluttering up your memory with too many character drawings."
+    },
+    {
+      id: 17,
+      kanji: '吾',
+      keyword: "I",
+      heisig: "There are actually a number of kanji for the word I, but the others tend to be more specific than this one. The key word here should be taken in the general psychological sense of the perceiving subject. Now the one place in our bodies that all five senses are concentrated in is the head, which has no less than five mouths: 2 nostrils, 2 ears, and 1 mouth. Hence, five mouths = I."
+    },
+    {
+      id: 18,
+      kanji: '冒',
+      keyword: "risk",
+      heisig: "Remember when you were young and your mother told you never to look directly into the sun for fear you might burn out your eyes? Probably you were foolish enough to risk a quick glance once or twice; but just as probably, you passed that bit of folk wisdom on to someone else as you grew older. Here, too, the kanji that has a sun above and an eye right below looking up at it has the meaning of risk."
+    },
+    {
+      id: 19,
+      kanji: '朋',
+      keyword: "companion",
+      heisig: "The first companion that God made, as the Bible story goes, was Eve. Upon seeing her, Adam exclaimed, Flesh of my flesh! And that is precisely what this character says in so many strokes."
+    },
+    {
+      id: 20,
+      kanji: '明',
+      keyword: "bright",
+      heisig: "Among nature's bright lights, there are two that the biblical myth has God set in the sky: the sun to rule over the day and the moon to rule the night. Each of them has come to represent one of the common connotations of this key word: the sun, the bright insight of the clear thinker, and the moon, the bright intuition of the poet and the seer."
+    },
 ]
 
 export default kanjiData
